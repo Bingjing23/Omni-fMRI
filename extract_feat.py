6306ede3f646617a75a8b8ba2a6110ebb0fb9f68
derrz2 <2586709080@qq.com>
2026-04-30 16:32:20 +0800

    Update extract_feat.py

--- extract_feat.py
+++ extract_feat.py
@@ -41,8 +41,8 @@ def build_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "--checkpoint",
         type=Path,
-        default=Path("pretrain_checkpoint/checkpoint_epoch_32.pth"),
-        help="Checkpoint path (default: pretrain_checkpoint/checkpoint_epoch_32.pth)",
+        default=Path("pretrain_checkpoint/checkpoint.pth"),
+        help="Checkpoint path (default: pretrain_checkpoint/checkpoint.pth)",
     )
     parser.add_argument(
         "--config",
